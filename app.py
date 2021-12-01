d55fababc35d9d2fab2fe619b23dbbbe5735adfb
ormktm96 <87331410+ormktm96@users.noreply.github.com>
2021-12-01 09:50:12 -0500

    Create app.py
    initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+FLASK_APP=microblog.py
+FLASK_ENV = development
+FLASK_DEBUG = 0
+* Serving Flask app 'microblog.py' (lazy loading)
+* Environment: production
+   WARNING: This is a development server. Do not use it in a production deployment.
+   Use a production WSGI server instead.
+* Debug mode: off
+* Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
